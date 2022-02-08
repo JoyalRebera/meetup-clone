a29e03b76d6d396e08a8fd9f494e279f8bd8fc16
joyal rebera <joyalrebera40@gmail.com>
2022-02-08 22:36:52 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,57 @@
+// swiper.js
+var swiper = new Swiper(".slider", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+     autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    
+    breakpoints: {
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+    },
+});
+
+
+var swiper = new Swiper(".popular", {
+    slidesPerView: 1,
+    spaceBetween: 5,
+     autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    
+    breakpoints: {
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+    },
+});
